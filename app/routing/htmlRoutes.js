fb0ac9b2a01497ee8698db0164f454c2efc373ce
Lauren Bullen <laurenbullen@comcast.net>
2019-03-20 21:35:01 -0700

    Modified ReadMe and some html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,6 +3,7 @@ module.exports = function(app){
     app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname, "/../public/survey.html"))
     });
+    // this routes to "home" if anything other than "/survey" is entered in URL
     app.get("*", function(req, res){
         res.sendFile(path.join(__dirname, "/../public/home.html"))
     })
